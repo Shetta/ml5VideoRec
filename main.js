992aaaa39e8bd9f4daee5116e4bc6bd60e02ed47
Ahmed Sheta <61049695+Shetta@users.noreply.github.com>
2021-01-28 00:18:21 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,34 +7,33 @@ var label = '';
 
 function modelReady() {
     console.log('Model is Ready!');
+    mobileNet.predict(gotResults);
 }
 
 function gotResults(error, results) {
     if (error) {
         console.log(error);
-    } 
-    else {
-        var name = results[0].label;
+    } else {
+        label = results[0].label;
         mobileNet.predict(gotResults);
     }
 }
 
 function setup() {
     createCanvas(640, 550);
-    background(0);
-
     //Capture the live feed
     video = createCapture(VIDEO);
     video.hide();
+    background(0);
 
     mobileNet = ml5.imageClassifier('MobileNet', video, modelReady);
-    
+
 }
 
 function draw() {
     background(0);
-    createCanvas(video, 0, 0);
-    fill(255);
+    image(video, 0, 0);
+    fill(200);
     textSize(32);
     text(label, 10, height - 20);
 }
